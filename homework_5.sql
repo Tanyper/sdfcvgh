818cfe4186b4ef6e97cf76d5e5780a5e5279802b
Tanyper <89872932+Tanyper@users.noreply.github.com>
2021-12-19 21:24:23 +0300

    Add files via upload

---
+++ homework_5.sql
@@ -0,0 +1,23 @@
+UPDATE users
+	SET created_at = NOW() AND updated_at = NOW()
+;
+
+ALTER TABLE users MODIFY COLUMN created_at varchar(150); # преобразуем колонку в VARCHAR
+ALTER TABLE users MODIFY COLUMN updated_at varchar(150); # преобразуем колонку в VARCHAR
+
+
+UPDATE users
+	SET created_at = STR_TO_DATE(created_at, '%d.%m.%Y %k:%i'),
+	updated_at = STR_TO_DATE(updated_at, '%d.%m.%Y %k:%i')
+;
+
+
+ALTER TABLE users
+	MODIFY COLUMN created_at DATETIME,
+	MODIFY COLUMN updated_at DATETIME
+;
+
+UPDATE users
+	SET created_at = DATE_FORMAT(created_at, '%d.%m.%Y %H:%i'),
+	updated_at = DATE_FORMAT(updated_at, '%d.%m.%Y %H:%i')
+;
